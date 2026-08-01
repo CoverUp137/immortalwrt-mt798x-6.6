f57acd78e27130e524bd7b1e2a46384e68ea90a2
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-08-02 05:13:25 +0800

    Update mt7986.mk

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -523,6 +523,7 @@ define Device/netcore_n60-pro
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
